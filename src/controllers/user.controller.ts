755782d455de02d3017291dc7164a98bfb24c7b9
sinazay123 <sinazay123@gmail.com>
2022-12-09 20:23:34 -0500

    Added functionality to the create user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,21 @@
 import { Response, Request } from "express";
 import { CreateUserInput } from "../schemas/user.schema";
+import { createUserService } from "../services/user.service";
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput>,
   res: Response
 ) {
   const body = req.body;
+
+  try {
+    const user = await createUserService(body);
+
+    return res.send("User successfully created.");
+  } catch (err: any) {
+    if (err.code === 11000) {
+      return res.status(409).send("Account already exists.");
+    }
+    return res.status(500).send(err);
+  }
 }
