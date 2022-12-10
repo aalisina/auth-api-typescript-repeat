fae46accd4cacb68280c7f7cb03525566314b0d3
sinazay123 <sinazay123@gmail.com>
2022-12-09 19:25:12 -0500

    added router to app

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import express from "express";
 import config from "config";
 import connectToDb from "./utils/dbConnection";
 import log from "./utils/logger";
+import router from "./routes";
 
 const app = express();
+app.use(router);
 
 const PORT = config.get("port");
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,5 @@ import express from "express";
 const router = express.Router();
 
 router.get("/healthcheck", (_, res) => res.sendStatus(200));
+
+export default router;
