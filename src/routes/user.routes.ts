5b54573b3c5ec82e7cf636c9e94f1b21a8106f02
sinazay123 <sinazay123@gmail.com>
2022-12-09 20:17:04 -0500

    added validation and controller to the create user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import { createUserHandler } from "../controllers/user.controller";
+import validateResource from "../middlewares/validateResource";
+import { createUserSchema } from "../schemas/user.schema";
 
 const router = express.Router();
+router.post(
+  "/api/users",
+  validateResource(createUserSchema),
+  createUserHandler
+);
 
 export default router;
