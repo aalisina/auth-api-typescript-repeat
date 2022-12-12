c79edd3622fc060056497becedd25293ba9aa950
sinazay123 <sinazay123@gmail.com>
2022-12-12 12:13:04 -0500

    added testcreds to config

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,21 @@ export default {
   port: 3000,
   dbUri: "mongodb://localhost:27017/auth-api-repeat",
   logLevel: "info",
+  smtp: {
+    // just for testing purposes
+    user: "ujw2wejakuux7bhg@ethereal.email",
+    pass: "mWKGKadMf5ZAhSwqw2",
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+  },
 };
+
+// const testCreds = {
+//   user: "ujw2wejakuux7bhg@ethereal.email",
+//   pass: "mWKGKadMf5ZAhSwqw2",
+//   smtp: { host: "smtp.ethereal.email", port: 587, secure: false },
+//   imap: { host: "imap.ethereal.email", port: 993, secure: true },
+//   pop3: { host: "pop3.ethereal.email", port: 995, secure: true },
+//   web: "https://ethereal.email",
+// };
