6f146c23d8cba9c00e3cbb3a6d335d2bedeb21cf
sinazay123 <sinazay123@gmail.com>
2022-12-12 12:09:46 -0500

    added test creds for nodemailer

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from "express";
 import { CreateUserInput } from "../schemas/user.schema";
 import { createUserService } from "../services/user.service";
+import sendEmail from "../utils/mailer";
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput>,
@@ -12,6 +13,7 @@ export async function createUserHandler(
     const user = await createUserService(body);
 
     return res.send("User successfully created.");
+    await sendEmail();
   } catch (err: any) {
     if (err.code === 11000) {
       return res.status(409).send("Account already exists.");
