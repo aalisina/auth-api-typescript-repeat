956d46aca6504679a421107ad1144c0adf68212f
sinazay123 <sinazay123@gmail.com>
2022-12-12 20:03:09 -0500

    added find user by id service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,3 +3,7 @@ import UserModel, { User } from "../models/user.model";
 export async function createUserService(input: Partial<User>) {
   return UserModel.create(input);
 }
+
+export async function findUserByIdService(id: string) {
+  return UserModel.findById(id);
+}
